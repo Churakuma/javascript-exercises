8a3a404cfdb3a7269223dbde760b215524e95edc
Zubair <zubair99ss@hotmail.com>
2023-12-19 16:41:03 +0800

    Complete exercises 05_sumAll and 06_leapYears

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
+const sumAll = function(min, max) {
+    let result = 0;
 
+    if (min < 0 || max < 0) return "ERROR";
+    if (!Number.isInteger(min) || !Number.isInteger(max)) return "ERROR"
+    if (min > max) {
+        [min, max] = [max, min]
+    }
+    
+    for (let index = min; index <= max; index++) {
+        result += index;
+    }
+    return result;
 };
 
 // Do not edit below this line
