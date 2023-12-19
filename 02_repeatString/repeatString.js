e0216998f39fa8788abe5dc181beadc0b345c516
Zubair <zubair99ss@hotmail.com>
2023-12-19 15:21:07 +0800

    Complete exercise 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,18 @@
-const repeatString = function() {
+const repeatString = function(text, repetitions) {
+    let result = ""
 
+    if (repetitions >= 0) {
+        while (repetitions > 0) {
+        result += text;
+        repetitions--;
+        }
+    } else {
+        result = "ERROR"
+    }
+
+    
+
+    return result;
 };
 
 // Do not edit below this line
